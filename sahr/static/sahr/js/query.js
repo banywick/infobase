0fbc604943f81684d8539885d86fe5cd76a2e1b4
BanyWick <android19377@gmail.com>
2025-02-06 16:13:49 +0300

    работа над модулем САХР, js и тестовая страница

---
+++ sahr/static/sahr/js/query.js
@@ -0,0 +1,92 @@
+// scripts.js
+
+// Функция для получения CSRF-токена из мета-тега
+function getCSRFToken() {
+    const metaTag = document.querySelector('meta[name="csrf-token"]');
+    return metaTag ? metaTag.getAttribute('content') : '';
+}
+
+// Обработчик отправки формы поиска
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Предотвращаем отправку формы
+
+    const query = document.querySelector('input[name="query"]').value;
+    const csrfToken = getCSRFToken();
+
+    fetch('/sahr/sahr_find/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfToken // Отправляем CSRF-токен в заголовке
+        },
+        body: JSON.stringify({ query: query })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        // Здесь вы можете обработать ответ от сервера
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
+
+// Обработчик ввода в поле артикула
+const check_article = document.getElementById('article');
+const views_title = document.getElementById('title');
+const selectElement = document.getElementById('party');
+const hidden_id = document.createElement('input'); // Если нужен скрытый инпут для ID
+hidden_id.type = 'hidden';
+hidden_id.name = 'id';
+document.getElementById('articleForm').appendChild(hidden_id);
+
+check_article.addEventListener('input', async function () {
+    const enteredArticle = check_article.value;
+    console.log(enteredArticle);
+
+    try {
+        if (enteredArticle.trim() === '') {
+            // Если в инпуте нет значений, очищаем название
+            views_title.value = '';
+            selectElement.innerHTML = '';
+        } else {
+            const response = await fetch(`/sahr/check-article_form/${enteredArticle}/`);
+            const data = await response.json();
+            console.log(data);
+
+            selectElement.innerHTML = '';
+            for (let i in data.party) {
+                const option = document.createElement('option');
+                option.value = data.party[i];
+                option.text = data.party[i];
+                selectElement.appendChild(option);
+            }
+            // Отображаем название рядом с инпутом
+            if (data.title) {
+                views_title.value = data.title;
+                hidden_id.value = data.id;
+            }
+            if (data.error) {
+                views_title.value = data.error;
+            }
+        }
+    } catch (error) {
+        console.error('Ошибка при выполнении fetch-запроса:', error);
+    }
+});
+
+// Функция для отправки формы артикула
+function submitArticleForm() {
+    const formData = new FormData(document.getElementById('articleForm'));
+    fetch('/api/save-article/', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Форма успешно отправлена!');
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+}
--- sahr/views.py
+++ sahr/views.py
@@ -1,25 +1,54 @@
-from django.db.models import Q
+from django.db.models import Q, Sum
 from rest_framework.views import APIView
-from rest_framework.generics import CreateAPIView, DestroyAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView
 from django.views.generic import TemplateView
 from rest_framework import generics, status
-from .serializers import SahrDataTableSerializer
+
+from finder.models import Remains
+from .serializers import HistorySerializer, SahrDataTableSerializer
 from .models import *
 from rest_framework.response import Response
 
 
 
 class BasePositionView(generics.GenericAPIView):
-    queryset = Data_Table.objects.all()[:100]
+    queryset = Data_Table.objects.all()
     serializer_class = SahrDataTableSerializer
     lookup_field = 'id'
 
 class SahrView(TemplateView):
+    """Главная страница САХР"""
     template_name = 'sahr/index.html'
 
-class GetSahrAllPositions(BasePositionView, generics.ListAPIView):
-    pass
+class SahrArchiveView(TemplateView):
+    """Страница с архивом"""
+    template_name = 'sahr/archive.html'
 
+class AllArchiveRemovePosition(generics.ListAPIView):
+    """Все позиции с архива"""
+
+    queryset = Deleted.objects.all()
+    serializer_class = SahrDataTableSerializer
+
+
+class SahrFindFilterArchive(APIView):
+    """Поиск в архиве"""
+    def post(self, request, *args, **kwargs):
+        query = request.data.get('query', '')
+
+        # Создаем Q-объекты для каждого поля
+        article = Q(article__icontains=query)
+        party = Q(party__icontains=query)
+        address = Q(address__icontains=query)
+
+        # Объединяем Q-объекты с помощью логического "или"
+        combined_query = article | party | address
+
+        # Фильтруем данные в таблице
+        results = Deleted.objects.filter(combined_query)
+
+        # Возвращаем результаты в формате JSON
+        return Response(results.values())
 
 
 class SahrFindFilter(APIView):
@@ -39,6 +68,11 @@ class SahrFindFilter(APIView):
 
         # Возвращаем результаты в формате JSON
         return Response(results.values())
+    
+    
+    
+class GetSahrAllPositions(BasePositionView, generics.ListAPIView):
+    pass
 
 
 
@@ -59,11 +93,36 @@ class RemovePosition(BasePositionView, DestroyAPIView):
 
 
 
-class ArchiveRemovePosition(generics.ListAPIView):
-    queryset = Deleted.objects.all()
-    serializer_class = SahrDataTableSerializer
+class HistoryListView(ListAPIView):
+    serializer_class = HistorySerializer
 
+    def get_queryset(self):
+        data_table_id = self.kwargs['id']
+        return History.objects.filter(data_table_id=data_table_id)
+    
 
-class HistiryEditPositions(generics.ListAPIView):
-    queryset = Deleted.objects.all()
-    serializer_class = SahrDataTableSerializer
+class CheckArticleAPIView(APIView):
+    def get(self, request, art):
+        """Проверка, есть ли такой артикул в базе данных."""
+        article = Remains.objects.filter(article__icontains=art).first()
+        if article:
+            total_quantity = Remains.objects.filter(article=article).aggregate(sum_quantity=Sum('quantity'))['sum_quantity']
+            all_party = Remains.objects.filter(article=article)
+            party = {i: p.party for i, p in enumerate(all_party)}
+            title = article.title
+            unit = article.base_unit
+            art_product = article.article
+            project = article.project
+            id = article.id
+            total_quantity = f'{total_quantity:.2f}'
+            return Response({
+                "title": title,
+                "id": id,
+                "party": party,
+                'total_quantity': total_quantity,
+                'unit': unit,
+                'article': art_product,
+                "project": project
+            })
+        return Response({"error": "Товара нет в базе"}, status=status.HTTP_404_NOT_FOUND)    
+    
