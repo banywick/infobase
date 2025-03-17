604c9a39b64a3b244c4eb4f290abb9cb6deaaf22
banywick <android19377@gmail.com>
2025-03-17 10:45:16 +0300

    Добавлена инструкция

---
+++ finder/static/finder/js/popups.js
@@ -0,0 +1,11 @@
+
+// скрываем детализацию по клику
+// Находим элементы по их id
+const hideInfoButton = document.getElementById('hide_info');
+const detailsItem = document.getElementById('details_item');
+
+// Добавляем обработчик события на клик
+hideInfoButton.addEventListener('click', function() {
+    // Скрываем блок details_item
+    detailsItem.style.display = 'none';
+});
