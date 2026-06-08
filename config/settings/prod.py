3fda7456b0a09c6a40d213e8dea70abedb666657
Алексей Белоус <android19377@gmail.com>
2026-06-08 10:24:02 +0300

    отредактирован allowed

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 DEBUG = False
-ALLOWED_HOSTS = ['infobase-01.okbtsp.corp', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['infobase.okbtsp.com', 'localhost', '127.0.0.1']
 
 # CSRF_COOKIE_SECURE = True    # Передавать CSRF-куки только по HTTPS
 # SESSION_COOKIE_SECURE = True # Передавать сессионные куки только по HTTPS
